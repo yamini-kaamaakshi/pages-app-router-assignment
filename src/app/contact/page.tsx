6526594f283c479969416eb556d54748a77004b8
Yamini <yaminikaamaakshi@gmail.com>
2025-03-20 18:51:53 +0530

    updated contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { useState, useEffect } from "react";
-import Loading from "@/components/loading"; // Ensure this path is correct
+import Loading from "@/components/loading";
+import Link from "next/link"; // Ensure this path is correct
 
 export default function Contact() {
     const [loading, setLoading] = useState(true);
@@ -20,6 +21,14 @@ export default function Contact() {
             ) : (
                 <main className="text-center p-10">
                     <h1 className="text-2xl font-bold">Contact Us</h1>
+                    <Link
+                        href="https://www.linkedin.com/in/kaamaakshi-yamini-462b9b249/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 underline"
+                    >
+                        LinkedIn
+                    </Link>
                 </main>
             )}
         </>
